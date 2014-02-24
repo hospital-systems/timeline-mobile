fae99a74a7bca55ba562f05dcc8ddfaa49a3c669
Danil Kutkevich <danil@kutkevich.org>
2014-02-24 18:29:25 +0400

    angular repeat throw dummy collection

--- js/app.js
+++ js/app.js
@@ -8,5 +8,83 @@ angular.module('angular-timeline-demo', ['ngRoute'], function($routeProvider, $l
 });
 
 angular.module('angular-timeline-demo')
-    .controller('RootController', function($scope) {
-});
+  .controller('RootController', function($scope) {
+  });
+
+angular.module('angular-timeline-demo')
+  .controller('ListCtrl', function($scope) {
+    $scope.collectionOfDays = [
+      {
+        day: '2014-02-24',
+        records: [
+          {
+            type: 'notes',
+            name: 'Mango, Onion X., RN',
+            data: {
+              message: {
+                text: "Nursing Note:\nSeen and examined by Dr. Khoury with new orders noted and carried out."
+              }
+            }
+          },
+          {
+            type: 'vital_timeline_item',
+            name: 'Ugni molinae, Aubergine I., CNA',
+            data: {
+              title: 'Vital Signs:',
+              title2: 'Supine',
+              collection: [
+                {
+                  title: 'BP:',
+                  value: '96/49 Regular;'
+                },
+                {
+                  title: 'HR:',
+                  value: '95 /min;'
+                },
+                {
+                  title: 'RR:',
+                  value: '17 /min;'
+                },
+                {
+                  title: 'Temp F/C:',
+                  value: '97.6 F (36.4 C);'
+                },
+                {
+                  title: 'SpO<sub>2</sub>:',
+                  value: '95 %;'
+                },
+                {
+                  title: 'Pain:',
+                  value: '0;'
+                }
+              ]
+            }
+          }
+        ]
+      },
+      {
+        day: '2014-02-24',
+        records: [
+          {
+            type: 'intake_output_timeline_item',
+            name: 'Kiwi, Tomato F., CNA',
+            data: {
+              message: {
+                title: 'Output:',
+                text: 'Urine 200 ml foley'
+              }
+            }
+          },
+          {
+            type: 'treatment_record_timeline_item',
+            name: 'Coconut, Beet M., RT',
+            data: {
+              message: {
+                text: 'RCP Treatment Record'
+              }
+            }
+          }
+        ]
+      }
+    ];
+  });
