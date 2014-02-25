437f74ca39733895659f0570d7ca9d6e0a838392
Danil Kutkevich <danil@kutkevich.org>
2014-02-25 17:40:24 +0400

    day and time align

--- js/templates.js
+++ js/templates.js
@@ -21,7 +21,7 @@ angular.module('angular-timeline-demo').run(['$templateCache', function($templat
     "</div>\n" +
     "<div class=\"container timeline-frame\" ng-controller=\"ListCtrl\">\n" +
     "  <div class=\"timeline-container\" ng-repeat=\"(day, items) in days\">\n" +
-    "    <h2>{{getDayFromKey(day) | date:'MMM d, yyyy'}}</h2>\n" +
+    "    <h2 class=\"timeline-item-day\">{{getDayFromKey(day) | date:'MMM d, yyyy'}}</h2>\n" +
     "    <ul class=\"list-unstyled\">\n" +
     "      <li class=\"timeline-item\" ng-repeat=\"item in items\">\n" +
     "        <a class=\"timeline-item-link\" href=\"#/item/{{ item.id }}\">\n" +
@@ -33,8 +33,8 @@ angular.module('angular-timeline-demo').run(['$templateCache', function($templat
     "              <h3 class=\"timeline-item-title\">{{item.type}}</h3>\n" +
     "              <span class=\"text-muted\">{{item.name}}</span>\n" +
     "            </div>\n" +
-    "            <div class=\"col-xs-3 col-md-1\">\n" +
-    "              <span class=\"text-muted\">\n" +
+    "            <div class=\"timeline-item-time-container col-xs-3 col-md-1\">\n" +
+    "              <span class=\"timeline-item-time text-muted\">\n" +
     "                {{item.createdAt | date:'HH:mm'}}\n" +
     "              </span>\n" +
     "            </div>\n" +
