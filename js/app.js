bcb5d1c52a8120e5c359e1c77a8f60f99a8e39f1
Danil Kutkevich <danil@kutkevich.org>
2014-02-25 15:09:11 +0400

    dummy item controller

--- js/app.js
+++ js/app.js
@@ -1,6 +1,82 @@
+var collectionOfDays = [
+  {
+    day: 'Feb 24, 2014',
+    records: [
+      {
+        type: 'notes',
+        name: 'Mango, Onion X., RN',
+        data: {
+          message: {
+            text: "Nursing Note:\nSeen and examined by Dr. Khoury with new orders noted and carried out."
+          }
+        }
+      },
+      {
+        type: 'vital_timeline_item',
+        name: 'Ugni molinae, Aubergine I., CNA',
+        data: {
+          title: 'Vital Signs:',
+          title2: 'Supine',
+          collection: [
+            {
+              title: 'BP:',
+              value: '96/49 Regular;'
+            },
+            {
+              title: 'HR:',
+              value: '95 /min;'
+            },
+            {
+              title: 'RR:',
+              value: '17 /min;'
+            },
+            {
+              title: 'Temp F/C:',
+              value: '97.6 F (36.4 C);'
+            },
+            {
+              title: 'SpO<sub>2</sub>:',
+              value: '95 %;'
+            },
+            {
+              title: 'Pain:',
+              value: '0;'
+            }
+          ]
+        }
+      }
+    ]
+  },
+  {
+    day: 'Jan 13, 2014',
+    records: [
+      {
+        type: 'intake_output_timeline_item',
+        name: 'Kiwi, Tomato F., CNA',
+        data: {
+          message: {
+            title: 'Output:',
+            text: 'Urine 200 ml foley'
+          }
+        }
+      },
+      {
+        type: 'treatment_record_timeline_item',
+        name: 'Coconut, Beet M., RT',
+        data: {
+          message: {
+            text: 'RCP Treatment Record'
+          }
+        }
+      }
+    ]
+  }
+];
+
 timeline_app = angular.module('angular-timeline-demo', ['ngRoute']);
 
-timeline_app.config(['$routeProvider',
+timeline_app.config([
+  '$routeProvider',
   function($routeProvider) {
     'use strict';
 
@@ -8,84 +84,21 @@ timeline_app.config(['$routeProvider',
       templateUrl: '/templates/list.html',
       controller: 'RootController'
     });
+
+    $routeProvider.when('/item/:itemId', {
+      templateUrl: '/templates/item.html',
+      controller: 'RootController'
+    });
   }]);
 
 timeline_app.controller('RootController', function($scope) {
-  });
+});
+
+timeline_app.controller('ItemsCtrl', function($scope, $route, $routeParams) {
+  // $route.timelineItem = collectionOfDays[$routeParams.itemId];
+  $route.timelineItem = 1;
+});
 
 timeline_app.controller('ListCtrl', function($scope) {
-  $scope.collectionOfDays = [
-    {
-      day: 'Feb 24, 2014',
-      records: [
-        {
-          type: 'notes',
-          name: 'Mango, Onion X., RN',
-          data: {
-            message: {
-              text: "Nursing Note:\nSeen and examined by Dr. Khoury with new orders noted and carried out."
-            }
-          }
-        },
-        {
-          type: 'vital_timeline_item',
-          name: 'Ugni molinae, Aubergine I., CNA',
-          data: {
-            title: 'Vital Signs:',
-            title2: 'Supine',
-            collection: [
-              {
-                title: 'BP:',
-                value: '96/49 Regular;'
-              },
-              {
-                title: 'HR:',
-                value: '95 /min;'
-              },
-              {
-                title: 'RR:',
-                value: '17 /min;'
-              },
-              {
-                title: 'Temp F/C:',
-                value: '97.6 F (36.4 C);'
-              },
-              {
-                title: 'SpO<sub>2</sub>:',
-                value: '95 %;'
-              },
-              {
-                title: 'Pain:',
-                value: '0;'
-              }
-            ]
-          }
-        }
-      ]
-    },
-    {
-      day: 'Jan 13, 2014',
-      records: [
-        {
-          type: 'intake_output_timeline_item',
-          name: 'Kiwi, Tomato F., CNA',
-          data: {
-            message: {
-              title: 'Output:',
-              text: 'Urine 200 ml foley'
-            }
-          }
-        },
-        {
-          type: 'treatment_record_timeline_item',
-          name: 'Coconut, Beet M., RT',
-          data: {
-            message: {
-              text: 'RCP Treatment Record'
-            }
-          }
-        }
-      ]
-    }
-  ];
+  $scope.days = collectionOfDays;
 });
--- js/templates.js
+++ js/templates.js
@@ -1,24 +1,38 @@
 angular.module('angular-timeline-demo').run(['$templateCache', function($templateCache) {
   'use strict';
 
+  $templateCache.put('/templates/item.html',
+    "<div ng-controller=\"ItemsCtrl\">\n" +
+    "  <div>\n" +
+    "    foo\n" +
+    "  </div>\n" +
+    "  <div>\n" +
+    "    {{timelineItem}}\n" +
+    "  </div>\n" +
+    "</div>\n"
+  );
+
+
   $templateCache.put('/templates/list.html',
     "<div class=\"container-fluid timeline-frame\" ng-controller=\"ListCtrl\">\n" +
-    "  <div class=\"timeline-container\" ng-repeat=\"day in collectionOfDays\">\n" +
+    "  <div class=\"timeline-container\" ng-repeat=\"day in days\">\n" +
     "    <h2>{{day.day}}</h2>\n" +
     "    <ul class=\"list-unstyled\">\n" +
     "      <li class=\"timeline-item\" ng-repeat=\"item in day.records\">\n" +
-    "        <div class=\"row\">\n" +
-    "          <div class=\"col-xs-2 col-md-1\">\n" +
-    "            <span class=\"icon\" ng-class=\"'medapp-icon-' + item.type\"></span>\n" +
+    "        <a class=\"timeline-item-link\" href=\"#/item/{{ $index }}\">\n" +
+    "          <div class=\"row\">\n" +
+    "            <div class=\"col-xs-2 col-md-1\">\n" +
+    "              <span class=\"icon\" ng-class=\"'medapp-icon-' + item.type\"></span>\n" +
+    "            </div>\n" +
+    "            <div class=\"col-xs-8 col-md-10\">\n" +
+    "              <span>{{item.type}}</span>\n" +
+    "              <span class=\"text-muted\">{{item.name}}</span>\n" +
+    "            </div>\n" +
+    "            <div class=\"col-xs-2 col-md-1\">\n" +
+    "              <span class=\"text-muted\">5:30</span>\n" +
+    "            </div>\n" +
     "          </div>\n" +
-    "          <div class=\"col-xs-8 col-md-10\">\n" +
-    "            <span>{{item.type}}</span>\n" +
-    "            <span class=\"text-muted\">{{item.name}}</span>\n" +
-    "          </div>\n" +
-    "          <div class=\"col-xs-2 col-md-1\">\n" +
-    "            <span class=\"text-muted\">5:30</span>\n" +
-    "          </div>\n" +
-    "        </div>\n" +
+    "        </a>\n" +
     "      </li>\n" +
     "    </ul>\n" +
     "  </div>\n" +
