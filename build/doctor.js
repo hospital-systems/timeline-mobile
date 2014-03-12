7dde08f2e1b8dd83332bef977c34adfe41285edb
dmitry-n <nechayevdv@gmail.com>
2014-03-12 16:42:50 +0400

    add data to the profiles

--- build/doctor.js
+++ build/doctor.js
@@ -1,17 +1,35 @@
 var patients = patients || {};
 patients["MrBlonde"] = {
-  id: 2,
-  name: 'Mr. Blonde'
+    id: 2,
+    name: 'Mrs. Johnson',
+    fullname: 'Johnson, Jane',
+    gender: 'female',
+    date_of_birth: new Date(1948, 6, 12),
+    address: '518 Crestview Drive, Beverly Hills, CA',
+    phone: '555-0150',
+    email: 'jane-johnson@outlook.com'
 }
 ;
 patients["MrBrown"] = {
-  id: 1,
-  name: 'Mr. Brown'
+    id: 1,
+    name: 'Mr. Smith',
+    fullname: 'Smith, John',
+    gender: 'male',
+    date_of_birth: new Date(1934, 9, 27),
+    address: '9764 Jeopardy Lane, Chicago IL',
+    phone: '555-0101',
+    email: 'john_smith@yahoo.com'
 }
 ;
 patients["MrOrange"] = {
-  id: 3,
-  name: 'Mr. Orange'
+    id: 3,
+    name: 'Mrs. Major',
+    fullname: 'Major, Mary',
+    gender: 'female',
+    date_of_birth: new Date(1956, 7, 9),
+    address: '31 Spooner Street, Quahog, RI',
+    phone: '555-0199',
+    email: 'mary.major@gmail.com'
 }
 ;
 var mrBrownData = mrBrownData || {};
